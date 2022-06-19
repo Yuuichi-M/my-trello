2865e07a8068c0057f4cd3e8d531bf0cca4be0fe
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2022-06-19 14:50:41 +0900

    [add]カードの総数表示

---
+++ src/components/Board.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- ここにHTMLを書きます -->
+<div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+</template>
+
+<script>
+// ここにJSを書きます
+export default {
+}
+</script>
+
+<!-- 解説
+
+- export default : このコンポーネントを他のコンポーネントからimportして使用できるようになります
+
+
+-->
+
+<!-- プロセス
+
+- カードの総数表示
+- リストのドラッグ＆ドロップ
+
+-->
